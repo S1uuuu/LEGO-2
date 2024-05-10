fc5ac7e54373c3fba08073859ec082820eb15011
S1uuuu <150293603+S1uuuu@users.noreply.github.com>
2024-05-10 15:48:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+width:250,height:150, image_format:"png", image_quality:99  
+})
+Webcam.attach("#don")
+function meh (){
+    Webcam.snap(function(data_uri) { document.getElementById("mr").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>'; });    
+}
+console.log("ml5");
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/vZtps2k53/model.json",mkl);
+function mkl(){
+    console.log("lol");
+}
+function lol(){
+var burp=document.getElementById("selfie_image");
+classifier.classify(burp ,mama)
+}
+function mama(error,lala){
+if (error) {
+  console.log("error")  
+} else {
+    console.log("lala");
+document.getElementById("labelo").innerHTML=lala[0].label;
+document.getElementById("estoy") .innerHTML=lala[0].confidence;
+
+    
+}
+}
